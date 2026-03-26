355ef1132cee28b152683f1a1f4eef8c1b04bc55
kiyoon.yoo <kiyoon>
2026-03-26 14:27:55 +0900

    add: create entrypoint for eval

---
+++ scripts/eval.sh
@@ -0,0 +1,21 @@
+export CUDA_VISIBLE_DEVICES=0
+
+
+MODEL_DIR="sft-text/iter_1/final/full_model"
+OUTPUT_DIR="sft-text/iter_1"
+
+bash scripts/evaluate/evaluate_quick_test.sh \
+    --output-dir eval_results/$OUTPUT_DIR \
+    --num-trials 1 \
+    --num-traj-samples 5 \
+    --temperature 0.6 \
+    --adv-mode text \
+    --model-name outputs/$MODEL_DIR \
+    --adv-obs \
+    --adv-traj 
+
+
+
+    
+# --iteration-dir outputs/sft-overfit-3 \
+# --iteration 0
